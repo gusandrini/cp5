ecb5d5ec43b51605c6d88f6f5d00797fb1cbe635
dudatiemiak <rm554756@fiap.com.br>
2024-09-30 20:03:16 -0300

    Adicionado código types

---
+++ cp/src/types/types.tsx
@@ -0,0 +1,9 @@
+export interface NasaImage {
+    title: string;             
+    url: string;               
+    explanation: string;       
+    date: string;              
+    media_type: string;        
+  }
+  
+  
