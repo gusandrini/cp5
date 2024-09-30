90cd5285c5711a20c94cdda50e6f936950af8afe
gusandrini <gustavosandrini92@gmail.com>
2024-09-30 15:44:45 -0300

    corrigindo

--- cp/src/components/Cabecalho/Cabecalho.tsx
+++ cp/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,11 @@
+import Menu from "../Menu/Menu";
+
+
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
         <h1>Cabecalho</h1>
+        <Menu/>
     </header>
   )
 }
