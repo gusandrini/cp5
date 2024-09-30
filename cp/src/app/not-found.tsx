dce1104090fae0cf3520f8a151e69e9532b6070b
gusandrini <gustavosandrini92@gmail.com>
2024-09-30 18:33:16 -0300

    estilização da pagina de erro

--- cp/src/app/not-found.tsx
+++ cp/src/app/not-found.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-        <h1>NOT FOUND - 404</h1>
-        <h2>Página não encontrada...</h2>
+    <div className="not-found">
+      <Image
+        src="/images/erro-404.jpg" // Substitua pelo caminho da sua imagem
+        alt="Not Found"
+        width={800} // Defina a largura
+        height={800} // Defina a altura
+      />
+      <h1 className="text-4xl font-bold">404 - Página Não Encontrada</h1>
+      <p className="mt-2">Desculpe, a página que você está procurando não existe.</p>
+      <Link href="/">
+        Voltar para a página inicial
+      </Link>
     </div>
-  )
+  );
 }
