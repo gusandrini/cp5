6d753cda1be016833ffa3761403a032aa4e300cb
gusandrini <gustavosandrini92@gmail.com>
2024-09-30 15:39:26 -0300

    criado menu

---
+++ cp/src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <nav>
+        <ul>
+            <li> <Link href="/">Home</Link> </li>
+            <li> <Link href=""></Link> </li>
+            <li> <Link href=""></Link> </li>
+        </ul>
+    </nav>
+  )
+}
