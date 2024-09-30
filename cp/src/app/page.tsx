6ae1c8d6f0e513477283d299596f36de2f065c6f
gusandrini <gustavosandrini92@gmail.com>
2024-09-30 20:35:07 -0300

    adicionando link api

--- cp/src/app/page.tsx
+++ cp/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import Nasa from "./nasa/page";
+
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Livro Worlds in Collision, de Immanuel Velikovsky</h1>
+
+    <div className="container-ilu">
+
+        <div className="conteudo">
+        <Nasa/>
+        </div>
+
     </div>
+    
   )
 }
