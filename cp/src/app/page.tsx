faac232435da709b2ed26c8698bf7b1f72635279
dudatiemiak <rm554756@fiap.com.br>
2024-09-30 21:03:56 -0300

    Adicionando conteúdo home

--- cp/src/app/page.tsx
+++ cp/src/app/page.tsx
@@ -2,9 +2,10 @@
 export default function Home() {
   return (
 
-    <div className="container-ilu">
+    <div className="container">
 
         <div className="conteudo">
+          <p>Livro Worlds in Collision, de Immanuel Velikovsky</p>
         </div>
 
     </div>
